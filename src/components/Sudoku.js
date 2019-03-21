327234e021d003616a1eadc17e7d26e281d1bfb5
tyhchoi <tyhchoi@gmail.com>
2019-03-21 01:03:33 -0700

    added method to create random start from dataset

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import Grid from './Grid';
+import file from '../files/sudoku.json'
+
+const LENGTH = 9;
+const NUMVALS = 65500;
 
 class Sudoku extends Component {
   state = {
-    grid: []
+    grid: this.randomGrid()
+  }
+
+  randomGrid() {
+    const random = Math.floor(Math.random() * NUMVALS);
+    const data = file.data[random];
+    const grid = [];
+
+    for (let i = 0; i < LENGTH; i++) {
+      const row = [];
+      for (let j = 0; j < LENGTH; j++) {
+        const val = data[j + LENGTH * i];
+        if (val === '0') {
+          row.push('');
+        } else {
+          row.push(val);
+        }
+      }
+      grid.push(row);
+    }
+
+    return grid;
   }
 
   render() {
---
+++ src/components/createJson.js
@@ -0,0 +1,22 @@
+const parse = require('csv-parse')
+const fs = require('fs')
+
+const readFile = (file) => new Promise( ( resolve, reject ) => {
+  let result = [];
+  fs.createReadStream(file)
+  .pipe(parse())
+  .on('data', data => {
+    result.push(data[0])
+  })
+  .on('end', () => {
+    resolve(result)
+  })
+  .on('error', reject);
+});
+
+const createFile = async () => {
+  const data = await readFile('./sudoku.csv')
+  fs.writeFileSync('sudoku.json', JSON.stringify({data}))
+}
+
+createFile()
