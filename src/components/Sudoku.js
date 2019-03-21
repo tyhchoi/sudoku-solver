92104cd580a51362cc27e18489ca809d101da86e
tyhchoi <tyhchoi@gmail.com>
2019-03-21 11:47:09 -0700

    added solving function and button

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -31,12 +31,76 @@ class Sudoku extends Component {
     return grid;
   }
 
+  checkRowCol(grid, row, col, val) {
+    for (let i = 0; i < LENGTH; i++) {
+      if (grid[i][col] === val || grid[row][i] === val) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  checkSquare(grid, row, col, val) {
+    let rows = [0, 3];
+    let cols = [0, 3];
+    while (row >= rows[1]) {
+      rows[0] += 3;
+      rows[1] += 3;
+    }
+
+    while (col >= cols[1]) {
+      cols[0] += 3;
+      cols[1] += 3;
+    }
+
+    for (let i = rows[0]; i < rows[1]; i++) {
+      for (let j = cols[0]; j < cols[1]; j++) {
+        if (grid[i][j] === val) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  getNumbers(grid, row, col) {
+    const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    return nums.filter(num => this.checkRowCol(grid, row, col, num) && this.checkSquare(grid, row, col, num));
+  }
+
+  solve(grid) {
+    for (let i = 0; i < LENGTH; i++) {
+      for (let j = 0; j < LENGTH; j++) {
+        if (grid[i][j] === '') {
+          const nums = this.getNumbers(grid, i, j);
+          for (let k = 0; k < nums.length; k++) {
+            grid[i][j] = nums[k];
+            this.setState(grid);
+            if (!this.solve(grid)) {
+              grid[i][j] = ''
+              this.setState(grid);
+            } else {
+              break;
+            }
+          }
+
+          return !(grid[i][j] === '');
+        }
+      }
+    }
+
+    return true;
+  }
+
   render() {
     return (
       <div className="game">
-        <div className="game-board">
+        <div className="game-grid">
           <Grid grid={this.state.grid} />
         </div>
+        <button onClick={() => this.solve(this.state.grid)}>Solve</button>
       </div>
     );
   }
